d0c047658dfef3d7b287787cf474b5fc7df107d3
JesusSalasMataro <jesus459@hotmail.es>
2017-11-12 00:24:23 +0100

    Added console project

---
+++ FuturisticRestaurantKataConsoleUI/Program.cs
@@ -0,0 +1,46 @@
+﻿using FuturisticRestaurantKataTDD;
+using FuturisticRestaurantKataTDD.Contracts;
+using FuturisticRestaurantKataTDD.Implementations;
+using System;
+using System.Collections.Generic;
+
+namespace FuturisticRestaurantKataConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Table> tables = InitializeTables();
+            OccupyTables(tables);
+            Console.ReadKey();
+        }
+
+        private static List<Table> InitializeTables()
+        {
+            List<Table> tables = new List<Table>();
+            Observer waiter = new WaiterService();
+            Observer menu = new ElectronicMenuService();
+            Observer metre = new MetreService();
+
+            for (int i = 0; i < 5; i++)
+            {
+                bool oddIndex = (i + 1) % 2 != 0;
+                Table table = new Table(tableNumber: i + 1, isVip: oddIndex);
+                table.Attach(waiter);
+                table.Attach(menu);
+                table.Attach(metre);
+                tables.Add(table);
+            }
+
+            return tables;
+        }
+
+        private static void OccupyTables(List<Table> tables)
+        {
+            foreach (Table table in tables)
+            {
+                table.Status = TableStatusEnum.Occupied;
+            }
+        }
+    }
+}
